9d3807cb4c0693b36360702e895514c0a0d1c753
QLMio <153973485+QLMio@users.noreply.github.com>
2024-06-23 10:35:25 +0000

    更新 autodeploy.yml Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/12/hello-world/","2024/04/27/hello/","2024/05/03/share-web/"];function toRandomPost(){
+var posts=["2024/06/23/hello-world/","2024/04/27/hello/","2024/05/03/share-web/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
