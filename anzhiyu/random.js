a6dc056b3d15b617139bbd3a9bd5103ec539609d
QLMio <153973485+QLMio@users.noreply.github.com>
2024-08-05 15:28:56 +0000

    更新 Thanox Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/29/hello-world/","2024/04/27/hello/","2024/05/03/share-web/","2024/04/27/thanox/"];function toRandomPost(){
+var posts=["2024/08/05/hello-world/","2024/04/27/hello/","2024/05/03/share-web/","2024/04/27/thanox/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
